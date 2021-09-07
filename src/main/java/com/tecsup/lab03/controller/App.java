a4ff15db299c1bf73a897f8e2314ae1c363e3cef
Valeriano Daniel <daniel.valeriano@tecsup.edu.pe>
2021-09-07 11:49:57 -0500

    el primer commit

---
+++ src/main/java/com/tecsup/lab03/controller/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab03.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
